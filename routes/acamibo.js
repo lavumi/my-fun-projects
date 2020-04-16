8002fabe912f1b10ccd1998f923b1a710a11e367
DYKim <lavumi@flysher.net>
2020-04-16 19:41:21 +0900

    select menu ongoing

--- routes/acamibo.js
+++ routes/acamibo.js
@@ -8,20 +8,73 @@ var mysql = require('mysql');
 // });
 
 
+
 var connection = mysql.createConnection({
-    host  : 'lavumi.iptime.org',
-    user : 'development',
-    password : '1q2w3e',
-    database : 'ac_amibo_dev',
-    port : '13306'
+    host: 'lavumi.iptime.org',
+    user: 'development',
+    password: '1q2w3e',
+    database: 'ac_amibo_dev',
+    port: '13306'
 })
 
 
-router.get('/', function(req,res){
-    connection.query('SELECT * FROM villager'  
-     + ' where personality=3'
-     + ' order by birth'
-    , function(err, result, fileds){
+router.get('/', function (req, res) {
+    res.render('acamibo', {
+        title: 'Lavumi Animal Crossing Amibo',
+        results: []
+    });
+    // connection.query('SELECT * FROM villager;'
+    //     , function (err, result, fileds) {
+    //         if (err)
+    //             console.log(err);
+    //         else {
+    //             res.render('acamibo', {
+    //                 title: 'Lavumi Animal Crossing Amibo',
+    //                 results: []
+    //             });
+    //         }
+    //     });
+});
+
+
+router.get('/test', function (req, res) {
+    res.send('test');
+});
+
+// connection.connect();
+// connection.end();
+
+var selectquery = function (species, gender, personality, res) {
+
+    var querystring = 'SELECT * FROM villager';
+    if (species != '-' || gender >= 0 || personality >= 0) {
+        querystring += ' where';
+    }
+    var prefix = false;
+    if (species != '-' && species != '') {
+        querystring += ' species=\'' + species + '\'';
+        prefix = true;
+    }
+
+    if (gender >= 0) {
+        if (prefix === true)
+            querystring += ' and';
+        querystring += ' gender=' + gender;
+        prefix = true;
+    }
+
+    if (personality >= 0) {
+        if (prefix === true)
+            querystring += ' and';
+        querystring += ' personality=' + personality;
+    }
+
+
+    querystring += ';';
+
+    console.log(querystring);
+
+    connection.query(querystring, function(err, result, fileds){
         if(err)
             console.log(err);
         else{
@@ -31,17 +84,51 @@ router.get('/', function(req,res){
             });
         }
     });
+};
+
+router.post('/', function (req, res) {
+    console.log(req.body);
+
+    var species = req.body.species;
+    var gender;
+    if (req.body.gender === "-" || req.body.gender === "") {
+        gender = -1;
+    }
+    else if (req.body.gender === "남") {
+        gender = 0;
+    }
+    else if (req.body.gender === "여") {
+        gender = 1;
+    }
+
+    var personality;
+    if (req.body.personality === "-" || req.body.personality === "") {
+        personality = -1;
+    }
+    else if (req.body.personality === "먹보") {
+        personality = 0;
+    }
+    else if (req.body.personality === "운동광") {
+        personality = 1;
+    }
+    else if (req.body.personality === "무뚝뚝") {
+        personality = 2;
+    }
+    else if (req.body.personality === "느끼함") {
+        personality = 3;
+    }
+    else if (req.body.personality === "친절함") {
+        personality = 4;
+    }
+    else if (req.body.personality === "아이돌") {
+        personality = 5;
+    }
+    else if (req.body.personality === "성숙함") {
+        personality = 6;
+    }
+    else if (req.body.personality === "단순활발") {
+        personality = 7;
+    }
+    selectquery(species, gender,personality, res);
 });
-
-
-router.get('/test', function (req, res) {
-    res.send('test');
-  });
-// connection.connect();
-
-
-
-// connection.end();
-
-
 module.exports = router;
